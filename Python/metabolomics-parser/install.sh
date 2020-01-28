4b15eb5fc07151bfcf9c0e74aee13828ffc876d7
Scott Campit <scampit@umich.edu>
2020-01-28 14:45:50 -0500

    Update install.sh

--- Python/metabolomics-parser/install.sh
+++ Python/metabolomics-parser/install.sh
@@ -9,4 +9,3 @@ pip3 install -U -r requirements.txt --user
 
 echo "Finished making virtual environment and installing necessary depedencies."
 echo "To run the metabolomics parser, run './metabolomicsParser.sh [pathToMetabolomicsFile] [pathToMetabolicModel]' in the terminal."
-echo "For more questions, read the documentation at "
